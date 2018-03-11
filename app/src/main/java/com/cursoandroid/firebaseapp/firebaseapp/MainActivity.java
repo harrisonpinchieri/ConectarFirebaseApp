cd079b8b8f385d47681cbf31ed957ed70b23df59
Harrison <harrisonpinchieri@hotmail.com>
2018-03-11 03:54:16 -0300

    final

--- app/src/main/java/com/cursoandroid/firebaseapp/firebaseapp/MainActivity.java
+++ app/src/main/java/com/cursoandroid/firebaseapp/firebaseapp/MainActivity.java
@@ -2,19 +2,55 @@ package com.cursoandroid.firebaseapp.firebaseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference usuarioReferencia =  databaseReferencia.child("usuarios");
+    private DatabaseReference produtoReferencia =  databaseReferencia.child("produtos").child("001");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseReferencia.child("pontos").setValue("100");
+      /*  Usuario usuario = new Usuario();
+        Produto produto = new Produto();
+
+        usuario.setNome("Ana Helena");
+        usuario.setSobrenome("Silva");
+        usuario.setIdade(25);
+        usuario.setSexo("Feminino");
+
+
+        produto.setCor("branco");
+        produto.setDescricao("8 polegadas");
+        produto.setFabricante("apple");
+
+        usuarioReferencia.child("003").setValue(usuario);
+
+        produtoReferencia.child("001").setValue(produto);*/
+
+
+        produtoReferencia.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+               Log.i("FIREBASE",dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
